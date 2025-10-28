374cb32adc40864e2f4de15234ecfce37a00886b
Ahmet Berk Güneli <124775535+ArsenLupen53@users.noreply.github.com>
2025-10-28 23:53:18 +0300

    Add Morse code decoder function
    Implement a function to decode Morse code into plain text.

---
+++ main.py
@@ -0,0 +1,70 @@
+def decode_morse(morse_code):
+    #defining a dictionary  about morse code,letters
+    MORSE_CODE = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '.-.-.-': '.',
+    '--..--': ',',
+    '..--..': '?',
+    '-.-.--': '!',
+    '-..-.': '/',
+    '-.--.': '(',
+    '-.--.-': ')',
+    '.-...': '&',
+    '---...': ':',
+    '-.-.-.': ';',
+    '-...-': '=',
+    '.-.-.': '+',
+    '-....-': '-',
+    '..--.-': '_',
+    '.-..-.': '"',
+    '...-..-': '$',
+    '.--.-.': '@'
+}
+    new = "" #creating an empty string
+    morse_code = morse_code.strip() #reduce from spaces
+    words = morse_code.split("   ") #split the words
+    for i in words:
+        letters = i.split(" ") #split the letters
+        for a in range(len(letters)):
+            letters[a] = MORSE_CODE.get(letters[a],"") #getting the letter version of morse code
+        for z in letters:
+            new+=z #adding to string
+        new+=" " #create a space between words
+
+    return new[:-1] #reduce from the last space
+
